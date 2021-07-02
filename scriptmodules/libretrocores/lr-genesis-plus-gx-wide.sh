f0af350ddda3c809df00cf0cfa3279c8fa3dc7ff
Mr-Ugly-Hacks <ericrenfro34@yahoo.com>
2021-07-01 23:16:47 -0700

    Update lr-genesis-plus-gx-wide.sh
    typo

--- scriptmodules/libretrocores/lr-genesis-plus-gx-wide.sh
+++ scriptmodules/libretrocores/lr-genesis-plus-gx-wide.sh
@@ -9,7 +9,7 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="lr-genesis-plus-gx-wide-wide"
+rp_module_id="lr-genesis-plus-gx-wide"
 rp_module_desc="Sega 8/16 bit emu - Genesis Plus (enhanced) Widescreen port for libretro"
 rp_module_help="ROM Extensions: .bin .cue .gen .gg .iso .md .sg .smd .sms .zip\nCopy your Game Gear roms to $romdir/gamegear\nMasterSystem roms to $romdir/mastersystem\nMegadrive / Genesis roms to $romdir/megadrive\nSG-1000 roms to $romdir/sg-1000\nSegaCD roms to $romdir/segacd\nThe Sega CD requires the BIOS files bios_CD_U.bin and bios_CD_E.bin and bios_CD_J.bin copied to $biosdir"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/Genesis-Plus-GX-Wide/master/LICENSE.txt"
